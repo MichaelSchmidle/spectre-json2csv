b549aa0129caed28b6e0befd9f786d028ece16dd
Michael Schmidle <michael@schmidle.net>
2018-06-08 21:00:58 +0200

    First error-free version

--- spectre-json2csv.py
+++ spectre-json2csv.py
@@ -8,9 +8,23 @@ import csv
 baseDir = os.path.dirname(__file__) + '/'
 
 # Create array of JSON files to parse
-jsonFiles = glob.glob(baseDir + 'json/*.json')
+jsonFilePaths = glob.glob(baseDir + 'json/*.json')
 
 # Per JSON file
-for jsonFile in jsonFiles:
+for jsonFilePath in jsonFilePaths:
 
-    print(jsonFile)
+    # Open file and read metadata
+    with open(jsonFilePath) as jsonFile:
+        transactions = json.load(jsonFile)
+        account = transactions[0]['account_id']
+        closingDate = transactions[len(transactions) - 1]['made_on']
+
+        # Generate corresponding CSV file with header row
+        with open(baseDir + 'csv/' + str(account) + '-' + closingDate + '.csv', 'w') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(['Account ID', 'Amount', 'Currency', 'Date', 'Info', 'Reference'])
+
+            # Write each transaction as row in CSV file
+            for transaction in transactions:
+                writer.writerow([account, transaction['amount'], transaction['currency_code'], transaction['made_on'], transaction['description'], transaction['id']])
+
